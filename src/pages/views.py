651d224abc61df1759787e7f8aeda54599c6f9bf
Avutzhan <dautov92@list.ru>
2021-10-19 17:44:22 +0600

    contact view

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home_view(*args, **kwargs):  # python specific args
+def home_view(request, *args, **kwargs):  # python specific args
+    print(args, kwargs)
+    print(request.user)
     return HttpResponse("<h1>hello world</h1>")
+
+
+def contact_view(request, *args, **kwargs):  # python specific args
+    return HttpResponse("<h1>Contact world</h1>")
